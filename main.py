054d8048d65be1dd572d24a6beddf4e9cb137c89
Edgar Solis <edgar21dejunio@gmail.com>
2017-11-09 14:47:52 -0600

    Styling

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+
 class MainWindow(QWidget):
+    """Main window widget."""
+
     def __init__(self):
+        """Init."""
         super().__init__()
 
         self.initUi()
 
     def initUi(self):
+        """Ui Setup."""
+        pass
 
 
 app = QApplication(sys.argv)
