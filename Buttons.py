aa81f8de1051062b845b7002bb6ecd78e66cf8d8
Edgar Solis <edgar21dejunio@gmail.com>
2017-11-11 20:14:06 -0600

    Add Button class
    Added some simple rounded buttons with a customizable
    lable, they corners are rounded and they are fully
    customizable and easy to set up.

---
+++ Buttons.py
@@ -0,0 +1,41 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class MenuBtn(QAbstractButton):
+    """
+    Color is defined as Qt.'color'
+    style is a stylesheet as 'QLabel { color : color;'
+    """
+    def __init__(self, width, height, rounded, color, label, style ,parent=None):
+        """Init."""
+        super().__init__(parent)
+
+        self.width = width
+        self.height = height
+        self.rounded = rounded
+        self.color = QColor(color)
+        self.label = label
+        self.text = style
+
+        label = QLabel(self.label)
+        label.setStyleSheet(style)
+        label.setAlignment(Qt.AlignCenter)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+        self.setFixedSize(self.width, self.height)
+
+    def paintEvent(self, event):
+        color = self.color.lighter(150) if self.underMouse() else self.color
+        if self.isDown():
+            color = self.color.darker(150)
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        path = QPainterPath()
+        path.addRoundedRect(QRectF(0.0, 0.0, self.width, self.height),
+                            self.rounded, self.rounded)
+        painter.fillPath(path, color)
