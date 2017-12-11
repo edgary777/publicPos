171107f59ba27018b8cb36c86d06b875b17e954c
Edgar Solis <edgar21dejunio@gmail.com>
2017-12-11 16:57:28 -0600

    New module Dialogs
    The first Dialog to be created and used is the DctDialog.
    
    This dialog updates the discount in an order's OrderTotal.

---
+++ Dialogs.py
@@ -0,0 +1,103 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+class DctDialog(QDialog):
+    """Pop-Up window to set a discount for an order."""
+
+    def __init__(self, total, parent, percentage=None, amount=None, code=None):
+        """Init."""
+        super().__init__(parent)
+
+        self.parent = parent
+        self.total = total
+        self.newTotal = total
+        self.percentage = percentage
+        self.amount = amount
+        self.code = code
+
+        self.initUi()
+
+        self.newTotalUpdate()
+
+    def initUi(self):
+        """UI setup."""
+        self.newTotalLabel = QLabel(self.total)
+        btnOk = QPushButton("Aceptar")
+        btnCancel = QPushButton("Cancelar")
+
+        btnLayout = QHBoxLayout()
+        btnLayout.addWidget(btnOk)
+        btnLayout.addWidget(btnCancel)
+
+        layout = QVBoxLayout()
+        layout.addStretch()
+
+        items = {"percentage": "%:", "amount": "Cantidad:", "code": "Cupón:"}
+
+        for key, value in items.items():
+            setattr(self, key + "Layout", QHBoxLayout())
+            setattr(self, key + "Input", QLineEdit())
+            getattr(self, key + "Input").setPlaceholderText(getattr(self, key))
+            setattr(self, key + "Label", QLabel(value))
+            getattr(self, key + "Label").setAlignment(Qt.AlignRight)
+            getattr(self, key + "Layout").addWidget(getattr(self, key +
+                                                            "Label"))
+            getattr(self, key + "Layout").addWidget(getattr(self, key +
+                                                            "Input"))
+            layout.addLayout(getattr(self, key + "Layout"))
+
+        layout.addStretch()
+        layout.addWidget(self.newTotalLabel)
+        layout.addWidget(btnLayout)
+
+        self.setLayout(layout)
+
+        # self.setFixedWidth(300)
+        # self.setFixedHeight(300)
+
+    def setAmountDcto(self, dcto):
+        """Set discount amount."""
+        if dcto and dcto > 0:
+            self.amount = dcto
+        else:
+            self.amount = None
+        self.newTotalUpdate()
+
+    def setPercentageDcto(self, dcto):
+        """Set discount percentage."""
+        if dcto and dcto > 0:
+            self.percentage = dcto
+        else:
+            self.percentage = None
+        self.newTotalUpdate()
+
+    def setCodeDcto(self, code):
+        """Search and apply a code discount.
+
+        The search returns a list with 2 items, the first item is the type of
+        discount, the second item is the amount of discount.
+        """
+        pass
+
+    def newTotalUpdate(self):
+        """Update total with new discounts."""
+        if self.percentage or self.amount:
+            if self.amount:
+                self.newTotal = self.newTotal - self.amount
+
+            if self.percentage:
+                self.newTotal = self.newTotal * (self.percentage / 100)
+        else:
+            self.newTotal = self.total
+
+        self.newTotalLabel.setText(str(self.newTotal))
+
+    def returnDcto(self):
+        """Update the order with the discount."""
+        if self.newTotal != self.total:
+            dcto = 1 - (self.newTotal / self.total)
+        else:
+            dcto = None
+        self.parent.orderTotal.updateDcto(dcto)
