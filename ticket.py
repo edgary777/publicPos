7e1fb1b130c7c4a283338d06611b251bcd14ccb9
Edgar Solis <edgar21dejunio@gmail.com>
2017-12-27 14:35:29 -0600

    Ticket methods rearranged.
    Internal changes made in the class methods.

--- ticket.py
+++ ticket.py
@@ -6,18 +6,51 @@ from PyQt5.QtCore import *
 class Ticket(QWidget):
     """Ticket widget."""
 
-    def __init__(self, parent):
+    def __init__(self, data, parent, simplified=None):
         """Init."""
         super().__init__(parent)
 
-        self.parseData()
+        self.parseData(data)
 
-        self.initUi()
+        if simplified:
+            self.simplifiedTicket()
+        else:
+            self.ticket()
 
-    def initUi(self):
-        """Visualization is created here."""
+    def ticket(self):
+        """Ticket visualization is created here."""
         pass
 
-    def parseData(self):
-        """Parse and arrange the data for the ticket."""
+    def simplifiedTicket(self):
+        """Order visualization is created here."""
         pass
+
+    def parseData(self, data):
+        """Parse and organize the data for the ticket."""
+        self.image = None
+        self.title = None
+
+        self.address = None
+        self.regimenFiscal = None
+        self.RFC = None
+        self.name = None
+        self.tel = None
+
+        self.folio = None
+        self.date = None
+        self.hour = None
+
+        self.products = None
+
+        self.factura = None
+
+        self.total = None
+        self.subtotal = None
+        self.dcto = None
+        self.iva = None
+
+        self.notes = None
+
+        self.status = None  # PAG / LLEVA
+
+        self.takeOut = None  # AQUÍ / LLEVAR
