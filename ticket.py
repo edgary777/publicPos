8cc3190bf8ed60e96564af74cbb834b0a3cb5f30
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-04 15:24:52 -0600

    Add print support
    Added print support to the ticket and some tests for it.
    It still needs to change the lenght of the ticket
    dinamically, and it also needs to have a printer
    selected by default so it doesn't ask for it
    everytime.

--- ticket.py
+++ ticket.py
@@ -1,16 +1,20 @@
+import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5.QtPrintSupport import *
 
 
 class Ticket(QWidget):
     """Ticket widget."""
 
-    def __init__(self, data, parent, simplified=None):
+    def __init__(self, data=None, parent=None, simplified=None):
         """Init."""
         super().__init__(parent)
 
         self.parseData(data)
+
+        self.setFixedWidth(200)
 
         if simplified:
             self.simplifiedTicket()
@@ -19,7 +23,28 @@ class Ticket(QWidget):
 
     def ticket(self):
         """Ticket visualization is created here."""
-        pass
+        topData = QVBoxLayout()
+        if self.image:
+            label = QLabel()
+            image = QPixmap(self.image)
+
+            label.setPixmap(image.scaledToWidth(180, Qt.FastTransformation))
+            label.setMargin(0)
+            label.setAlignment(Qt.AlignCenter)
+
+            topData.setSpacing(0)
+            topData.addWidget(label)
+        else:
+            label = QLabel(self.title)
+            label.setAlignment(Qt.AlignCenter)
+            label.setFixedWidth(180)
+            label.setMargin(0)
+
+            topData.setSpacing(0)
+            topData.addWidget(label)
+
+        topData.addWidget(QLabel(self.address))
+        self.setLayout(topData)
 
     def simplifiedTicket(self):
         """Order visualization is created here."""
@@ -27,8 +52,8 @@ class Ticket(QWidget):
 
     def parseData(self, data):
         """Parse and organize the data for the ticket."""
-        self.image = None
-        self.title = None
+        self.image = """Resources/s-print.png"""
+        self.title = """SUPER LONCHES DE TORREON"""
 
         self.address = None
         self.regimenFiscal = None
@@ -54,3 +79,47 @@ class Ticket(QWidget):
         self.status = None  # PAG / LLEVA
 
         self.takeOut = None  # AQUÍ / LLEVAR
+
+    def Print(self):
+        """Print the widget."""
+        printer = QPrinter(QPrinter.HighResolution)
+        # pageSize = QPageSize(QSizeF(80, 80),
+        # QPageSize.Millimeter, name="test2", matchPolicy=QPageSize.ExactMatch)
+        # printer.setPageSize(pageSize)
+        dialog = QPrintDialog(printer,self)
+        if (dialog.exec_() != QDialog.Accepted):
+            return
+        painter = QPainter()
+        painter.begin(printer)
+
+        xscale = printer.pageRect().width() / self.width()
+        yscale = printer.pageRect().height() / self.height()
+        scale = min(xscale, yscale)
+
+        # Apparently this sets the page size, so the second size in translate
+        # is the one that changes the page length. to change it dinamically I
+        # have to get the size of the widget and set it to it.
+
+        painter.translate(printer.paperRect().x() + printer.pageRect().width()/2,
+                          300)
+
+        painter.scale(scale, scale)
+
+        painter.translate(-1 * self.width() / 2, -1 * self.height() / 2)
+        self.render(painter)
+        painter.end()
+
+    def paintEvent(self, event):
+        """Set window background color."""
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.setPalette(p)
+
+
+app = QApplication(sys.argv)
+window = Ticket()
+window.show()
+# window.showMaximized()
+window.Print()
+sys.exit(app.exec_())
