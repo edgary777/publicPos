6e2d033f01d63f67cb9afc435124edbee8f96df0
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-11 19:09:36 -0600

    fix

--- Printer.py
+++ Printer.py
@@ -34,9 +34,9 @@ class Print(object):
         """ Seiko Epson Corp. Receipt Printer (EPSON TM-T20) """
 
         p = Usb(0x04b8, 0x0e03, 0)
-        p.text("Hello World\n")
-        p.image("logo.gif")
-        p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
+        # p.text("Hello World\n")
+        p.image(pixmap)
+        # p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
         p.cut()
         # pr = escpos.escpos.Escpos()
         # pr = escpos.printer.Usb()
