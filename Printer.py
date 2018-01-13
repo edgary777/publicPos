3434ddf28f72432d655aff4301cb8398cfc22073
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-13 16:29:18 -0600

    Print on Raspberry Pi
    Most vendors don't have drivers for ARM processors,
    so the printer method was modified to add a direct method
    of communication with compatible printers.
    
    As the system can work both on pc's with the printer
    drivers and without it will first try to print without
    the drivers, if it is not setup, then it will fallback
    to try print with the drivers, if this fails too
    it will just do nothing.

--- Printer.py
+++ Printer.py
@@ -1,6 +1,9 @@
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtPrintSupport import *
+from escpos.printer import Usb
+from PIL import Image
+import io
 
 
 class Print(object):
@@ -11,6 +14,15 @@ class Print(object):
         pass
 
     def Print(self, dialog, simplified=None, other=None):
+        """Print."""
+        try:
+            self.printLinux(dialog)
+        except ValueError:
+            self.PrintWindows(dialog, simplified=None, other=None)
+        else:
+            pass
+
+    def PrintWindows(self, dialog, simplified=None, other=None):
         """Print the passed dialog."""
         printer = QPrinter(QPrinter.HighResolution)
         if simplified:
@@ -19,9 +31,6 @@ class Print(object):
             printer.setDocName(str(other))
         else:
             printer.setDocName("TICKET")
-        # dialog = QPrintDialog(printer,self)
-        # if (dialog.exec_() != QDialog.Accepted):
-        #     return
         painter = QPainter()
         p = dialog.palette()
         p.setColor(dialog.backgroundRole(), Qt.white)
@@ -40,3 +49,48 @@ class Print(object):
         painter.translate(-1 * dialog.width() / 2, -1 * dialog.height() / 2)
         dialog.render(painter)
         painter.end()
+
+    def printLinux(self, dialog):
+        """Print the passed dialog.
+
+        Linux doesn't have raspberry pi drivers for the printer, so if running
+        on a raspberry pi print with this method
+        """
+        painter = QPainter()
+
+        p = dialog.palette()
+        p.setColor(dialog.backgroundRole(), Qt.white)
+
+        dialog.setPalette(p)
+
+        width = dialog.getSize()[0]
+
+        height = dialog.getSize()[1]
+
+        scale = 2.2
+
+        pixmap = QImage(width * scale, height * scale, QImage.Format_Grayscale8)
+
+        painter.begin(pixmap)
+        painter.scale(scale, scale)
+        dialog.render(painter)
+        painter.end()
+
+        buffer = QBuffer()
+        buffer.open(QIODevice.ReadWrite)
+        pixmap.save(buffer, "PNG")
+        # pixmap.save("Hi.png", "PNG")  # just for testing
+
+        strio = io.BytesIO()
+        strio.write(buffer.data())
+        buffer.close()
+        strio.seek(0)
+        pil_im = Image.open(strio)
+
+        buffer = None
+
+        p = Usb(0x04b8, 0x0e03, 0)
+        p.image
+        p.set(align='center')
+        p.image(pil_im, impl="graphics", fragment_height=2000)
+        p.cut()
