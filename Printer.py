1c9af91b0c64a36cf7bebd46e6f0a1a024e76d2c
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-13 00:13:26 -0600

    Multi-OS printer method
    first attempt to a method that works on raspbian and windows.

--- Printer.py
+++ Printer.py
@@ -1,6 +1,10 @@
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtPrintSupport import *
+from escpos.printer import Usb
+from PIL import Image
+import io
+import Dialogs
 
 
 class Print(object):
@@ -11,6 +15,17 @@ class Print(object):
         pass
 
     def Print(self, dialog, simplified=None, other=None):
+        """Print."""
+        try:
+            self.PrintWindows(dialog, simplified=None, other=None)
+        except:
+            self.printLinux(dialog)
+        else:
+            dialog = Dialogs.QuestionDialog("ERROR AL IMPRIMIR")
+            if dialog.exec_():
+                return
+
+    def PrintWindows(self, dialog, simplified=None, other=None):
         """Print the passed dialog."""
         printer = QPrinter(QPrinter.HighResolution)
         if simplified:
@@ -19,9 +34,6 @@ class Print(object):
             printer.setDocName(str(other))
         else:
             printer.setDocName("TICKET")
-        # dialog = QPrintDialog(printer,self)
-        # if (dialog.exec_() != QDialog.Accepted):
-        #     return
         painter = QPainter()
         p = dialog.palette()
         p.setColor(dialog.backgroundRole(), Qt.white)
@@ -40,3 +52,38 @@ class Print(object):
         painter.translate(-1 * dialog.width() / 2, -1 * dialog.height() / 2)
         dialog.render(painter)
         painter.end()
+
+    def printLinux(self, dialog):
+        """Print the passed dialog.
+
+        Linux doesn't have raspberry pi drivers for the printer, so if running
+        on a raspberry pi print with this method
+        """
+        painter = QPainter()
+
+        p = dialog.palette()
+        p.setColor(dialog.backgroundRole(), Qt.white)
+
+        dialog.setPalette(p)
+
+        pixmap = QPixmap(dialog.width(), dialog.height())
+        pixmap = QImage(pixmap.toImage())
+
+        painter.begin(pixmap)
+        dialog.render(pixmap)
+        painter.end()
+
+        buffer = QBuffer()
+        buffer.open(QIODevice.ReadWrite)
+        pixmap.save(buffer, "PNG")
+
+        strio = io.BytesIO()
+        strio.write(buffer.data)
+        strio.write(buffer.data())
+        buffer.close()
+        strio.seek(0)
+        pil_im = Image.open(strio)
+
+        p = Usb(0x04b8, 0x0e03, 0)
+        p.image(pil_im)
+        p.cut()
